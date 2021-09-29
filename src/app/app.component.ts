b17676738c952f4a6cbb321f6058e62c20e45783
Valentin Voica <valentin.voica@cpp.canon>
2021-09-29 15:16:31 +0300

    error handling on subscription; unsubscribing to the subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { IDailyBnrReport } from './models/IDailyBnrReport'
 import { BnrService } from './services/bnr.service';
 import { XmlParsingService } from './services/xml-parsing.service';
+
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'cursBnr';
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   public report: IDailyBnrReport;
 
@@ -18,8 +23,17 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.bnrService.getData().subscribe(async (data) => {
-      this.report = await this.xmlParsingService.parseXml(data);
-    })
+    this.bnrService.getData()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(async (data) => {
+        this.report = await this.xmlParsingService.parseXml(data);
+      }, (err) => {
+        console.error("Error retrieving data from the server: ", err)
+      })
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
